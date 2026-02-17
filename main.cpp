6dfbaa55714af76ffea429d46d89036d126e2d48
samuelSchulthies <samuelschulthies@gmail.com>
2026-02-16 18:58:04 -0700

    completed testing rendering graphics to ppm

--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+using namespace std;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+int main() {
+    // Image
+    int imageWidth = 256;
+    int imageHeight = 256;
+
+    // Render
+    cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+    for (int i = 0; i < imageHeight; i++){
+        clog << "\rScanLines remaining: " << (imageHeight - i) << ' ' << flush;
+        for (int j = 0; j < imageWidth; j++){
+            auto r = double(i) / (imageWidth - 1);
+            auto g = double(j) / (imageHeight - 1);
+            auto b = 1.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            cout << ir << ' ' << ig << ' ' << ib << "\n";
+        }
     }
 
-    return 0;
-}
+    clog << "\rDone.                   \n";
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+
+}
